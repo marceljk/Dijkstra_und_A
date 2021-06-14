c4b157d99c662ebce98dcd0cb4d0fb06044c0ddd
Lukas Burkhardt <lburkhardt@stud.hs-heilbronn.de>
2021-06-14 23:16:12 +0200

    PanelHopsControl-Klasse geändert

--- src/PanelHopsControl.java
+++ src/PanelHopsControl.java
@@ -11,8 +11,17 @@ public class PanelHopsControl extends JPanel {
     private String asternHopString = "000";
     private String dijkstraHopString = "000";
 
+    private String asterngeprueftString = "000";
+    private String dijkstrageprueft = "000";
+
+    private JTextArea asterngepruefttxt = new JTextArea(asterngeprueftString);
+    private JTextArea dijkstragepruefttxt = new JTextArea(dijkstraHopString);
+
     private JTextArea aSternhoptxt = new JTextArea(asternHopString);
     private JTextArea dijkstrahoptxt = new JTextArea(dijkstraHopString);
+
+    private JLabel geprueft = new JLabel("geprüft");
+    private JLabel schritte = new JLabel("Schritte");
 
     //BORDER
     Border loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
@@ -22,22 +31,38 @@ public class PanelHopsControl extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(1, 1, 1, 1);
         this.setBorder(BorderFactory.createTitledBorder(loweredetched,"Schritte"));
-        this.setPreferredSize(new Dimension(180,60));
+        this.setPreferredSize(new Dimension(180,80));
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        this.add(geprueft,gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        this.add(schritte,gbc);
 
         gbc.gridx = 0;
-        gbc.gridy = 0;
+        gbc.gridy = 1;
         this.add(aSternHops = new JLabel("A* "),gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 0;
+        gbc.gridy = 1;
         this.add(aSternhoptxt,gbc);
 
-        gbc.gridx = 0;
+        gbc.gridx = 2;
         gbc.gridy = 1;
+        this.add(asterngepruefttxt,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
         this.add(dijkstraHops = new JLabel("Dijkstra "),gbc);
 
         gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         this.add(dijkstrahoptxt,gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        this.add(dijkstragepruefttxt,gbc);
     }
 }
