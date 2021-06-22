e8c3f4ba2f5e6de6496ed6793254f2afb2f64049
Lukas <lukas.burkhardt@yahoo.de>
2021-06-22 16:00:15 +0200

    änderung

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -11,14 +11,18 @@ public class Dijkstra implements Runnable{
     private HashMap<Node, Node> vorgänger;
 
     @Override
-    public void run() {
+    public void run() {                     // Berechnungen können während der Laufzeit der GUI weiterlaufen
         searchPath();
     }
 
-    public void setGui(BoardGUI gui){
+    public void setGui(BoardGUI gui){       // übernimmt die aktuelle GUI / Muster um den Dijkstra darauf berechnen lassen zu können.
         this.gui = gui;
     }
 
+    /**
+     * Bekommt den Knoten mit dem kleinsten Abstand vom Startpunkt
+     * @return
+     */
     private Node getMinDistNode() {
         double min = Double.MAX_VALUE;
         Node current = null;
@@ -38,39 +42,43 @@ public class Dijkstra implements Runnable{
         initialisiere();
 
         boolean search = true;
+        try {
+            while (!q.isEmpty() && search) {        // Solange Suche nicht beendet und kürzester Pfad nicht gefunden, wiederhole Suche
+                Node u = getMinDistNode();          //
+                q.remove(u);                        // Knoten mit niedrigsten Kosten wird von Liste entfernt.
 
-        while (!q.isEmpty() && search) {
-            Node u = getMinDistNode();
-            q.remove(u);
-
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            for(Node successor: u.getSuccessor()) {
-
-                if(!(successor.getType() == 3 || successor.getType() == 2 || successor.getType() == 1)){   //Prüft ob der Knoten nicht ein Start-, Zielfeld oder eine Wand ist.
-                    successor.setSearched(true);
+                try {
+                    Thread.sleep(10);          // Pause von 10 ms
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
-                gepruefte = 0;
-                for(int x = 0; x < gui.getBoard().length; x++) {
-                    for(int y = 0; y < gui.getBoard()[x].length; y++) {
-                        if(gui.getBoard()[x][y].isSearched()){
-                            gepruefte++;
+                for (Node successor : u.getSuccessor()) {     // Bekommt die Nachbarknoten von dem letzten betrachteten Knoten, welcher entfernt wurde.
+
+                    if (!(successor.getType() == 3 || successor.getType() == 2 || successor.getType() == 1)) {   //Prüft ob der Knoten nicht ein Start-, Zielfeld oder eine Wand ist.
+                        successor.setSearched(true);
+                    }
+
+                    gepruefte = 0;
+                    for (int x = 0; x < gui.getBoard().length; x++) {
+                        for (int y = 0; y < gui.getBoard()[x].length; y++) {
+                            if (gui.getBoard()[x][y].isSearched()) {
+                                gepruefte++;
+                            }
                         }
                     }
-                }
-                PanelHopsControl.setDijkstraGeprueft(gepruefte);
+                    PanelHopsControl.setDijkstraGeprueft(gepruefte);        // Zeigt an, wie viele Knoten bereits geprüft sind.
 
-                if(q.contains(successor)) {
-                    distanz_update(u, successor);
+                    if (q.contains(successor)) {
+                        distanz_update(u, successor);
+                    }
                 }
             }
+        }catch (NullPointerException ex) {
+
+        } finally {
+            erstelleKuerzestenPfad();
         }
-        erstelleKuerzestenPfad();
     }
 
     private void initialisiere() {
@@ -88,7 +96,6 @@ public class Dijkstra implements Runnable{
             }
         }
         abstand.put(gui.getBoard()[gui.getStartx()][gui.getStarty()], 0.0);
-
     }
 
     private void distanz_update(Node currentNode, Node successor) {
@@ -107,18 +114,24 @@ public class Dijkstra implements Runnable{
         ArrayList<Node> path = new ArrayList<>();
         Node finalNode = gui.getFinalNode();
         path.add(finalNode);
-        while (vorgänger.get(finalNode) != null) {
-            finalNode = vorgänger.get(finalNode);
-            path.add(finalNode);
-        }
-        System.out.println("Größe: "+path.size());
-        for(Node node:path) {
-            if( !( node.equals(gui.getBoard()[gui.getStartx()][gui.getStarty()]) || node.equals(gui.getFinalNode()) ) ){
-                node.setPath(true);
+        if(vorgänger.get(finalNode) == null) {
+            System.out.println("Kein Weg gefunden");
+        } else {
+            while (vorgänger.get(finalNode) != null) {
+                finalNode = vorgänger.get(finalNode);
+                path.add(finalNode);
             }
+            System.out.println("Größe: "+path.size());
+            for(Node node:path) {
+                if( !( node.equals(gui.getBoard()[gui.getStartx()][gui.getStarty()]) || node.equals(gui.getFinalNode()) ) ){
+                    node.setPath(true);
+                }
+            }
+            System.out.println("Kosten: "+ gui.getFinalNode().getCostFromStart());
+            PanelHopsControl.setDijkstraHopTxt(gui.getFinalNode().getCostFromStart());
+            System.out.println("Weg gefunden");
         }
-        System.out.println("Kosten: "+ gui.getFinalNode().getCostFromStart());
-        PanelHopsControl.setDijkstraHopTxt(gui.getFinalNode().getCostFromStart());
+
     }
 
 }
